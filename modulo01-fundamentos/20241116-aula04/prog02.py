fd2013e6105f9656687927c8eb7d0f4c2d2b0930
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-16 15:40:28 -0300

    Arquivos aula 04 python fundamentos

---
+++ modulo01-fundamentos/20241116-aula04/prog02.py
@@ -0,0 +1,34 @@
+"""
+Entrada e Saída (I/O) de arquivos em Python
+
+Escrita de arquivos .txt
+"""
+
+from random import randint
+
+import os
+
+if __name__ == "__main__":
+    
+    numeros = [
+        str(randint(0, 999999)).zfill(6),
+        str(randint(0, 999999)).zfill(6),
+        str(randint(0, 999999)).zfill(6),
+        str(randint(0, 999999)).zfill(6),
+        str(randint(0, 999999)).zfill(6),
+    ]
+
+    # O método de string zfill() preenche com zeros à esquerda o valor especificado
+
+    caminho_pasta_saida = os.path.join(os.getcwd(), "saida")
+
+    # A função exists do modulo os.path verifica se um arquivo ou pasta existe. Se sim, retorna True
+    if not os.path.exists(caminho_pasta_saida):
+        # A função mkdir do pacote os cria uma pasta no caminho especificado
+        os.mkdir(caminho_pasta_saida)
+
+    # Aqui estamos abrindo o arquivo numeros.txt que está dentro da pasta saida, no modo de escrita. Se o arquivo não existir, será criado.
+    arquivo_numeros = os.path.join(caminho_pasta_saida, "numeros.txt")
+    with open(arquivo_numeros, "w", encoding="utf-8") as arquivo:
+        for numero in numeros:
+            arquivo.write(f"{numero}\n")
