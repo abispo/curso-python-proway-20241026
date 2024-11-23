474faadb95325c5e8a1310ccbdd62631fee092b4
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-23 16:41:02 -0300

    Arquivos aula 01 python com banco de dados

--- modulo02-python-com-banco-de-dados/20241123-aula01/prog02.py
+++ modulo02-python-com-banco-de-dados/20241123-aula01/prog02.py
@@ -1,4 +1,10 @@
+# 1. Importar os módulos/pacotes da biblioteca padrão
+# 2. Importar os módulos/pacotes instalados via pip
+# 3. Importar os módulos/pacotes do próprio projeto
+
 import csv
+import os
+
 import pymysql
 import pymysql.cursors
 import requests
@@ -11,7 +17,7 @@ if __name__ == "__main__":
         password="admin",
         database="modulo02_python",
         charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor
+        cursorclass=pymysql.cursors.DictCursor  # Retora uma lista de dicionários como resposta
     )
 
     cursor = connection.cursor()
@@ -29,4 +35,39 @@ if __name__ == "__main__":
 
     cursor.execute(comando)
 
+    # URL de onde o conteúdo será baixado
     url_arquivo_cursos = "https://raw.githubusercontent.com/abispo/shared-files/refs/heads/main/modulo02/cursos.csv"
+
+    # O método get da biblioteca requests acessa um endpoint (endereço) e baixa o seu conteúdo, independente de qual conteúdo seja
+    resposta = requests.get(
+        url_arquivo_cursos
+    )
+
+    pasta_arquivos = os.path.join(os.getcwd(), "arquivos")
+
+    if not os.path.exists(pasta_arquivos):
+        os.mkdir(pasta_arquivos)
+
+    with open(os.path.join(pasta_arquivos, "cursos.csv"), "w", encoding="utf-8") as arquivo:
+        arquivo.write(resposta.text)
+
+    with open(os.path.join(pasta_arquivos, "cursos.csv"), "r", encoding="utf-8") as arquivo:
+
+        arquivo_csv = csv.DictReader(arquivo, delimiter=';')
+
+        for linha in arquivo_csv:
+            comando = "INSERT INTO `tb_cursos` (`curso`, `carga_horaria`, `preco`) VALUES (%s, %s, %s);"
+            cursor.execute(
+                comando,
+                (
+                    linha.get("curso"),
+                    int(linha.get("carga_horaria")),
+                    float(linha.get("preco")),
+                )
+            )
+
+            connection.commit()
+
+    comando = "SELECT * FROM tb_cursos"
+    cursor.execute(comando)
+    print(cursor.fetchall())
