3e3d181a28f1649440603ec0f61d7037a9263584
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-23 16:04:48 -0300

    Arquivos aula 01 python com banco de dados

---
+++ modulo02-python-com-banco-de-dados/20241123-aula01/prog02.py
@@ -0,0 +1,32 @@
+import csv
+import pymysql
+import pymysql.cursors
+import requests
+
+if __name__ == "__main__":
+    
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="admin",
+        database="modulo02_python",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS tb_cursos;")
+
+    comando = """
+        CREATE TABLE tb_cursos(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            curso VARCHAR(100) NOT NULL,
+            carga_horaria TINYINT UNSIGNED NOT NULL,
+            preco FLOAT NOT NULL
+        );
+"""
+
+    cursor.execute(comando)
+
+    url_cursor = "https://raw.githubusercontent.com/abispo/shared-files/refs/heads/main/modulo02/cursos.csvs"
