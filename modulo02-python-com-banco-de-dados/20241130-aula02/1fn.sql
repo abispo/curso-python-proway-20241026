2360b0da4ac655c60cb5ee355ab0ccc64f7fa21b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-30 14:02:08 -0300

    arquivos aula 02 python com banco de dados

---
+++ modulo02-python-com-banco-de-dados/20241130-aula02/1fn.sql
@@ -0,0 +1,12 @@
+/*
+Primeira Forma Normal (1FN)
+
+A 1FN define que uma tabela deve ter AS seguintes regras:
+ * Cada coluna deve ter apenas valores atômicos ou indivisíveis, ou seja, 
+valores não compostos.
+ * A tabela deve ter pelo menos 1 coluna que seja chave primária.
+ * Não podemos ter na tabela colunas multivaloradas, ou seja, mais de
+um valor na mesma coluna.
+*/
+
+CREATE DATABASE IF NOT EXISTS 20241130_modulo02_python;
