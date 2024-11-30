79da629c12f0b0e0caee8a6b0fe0f0bb8525f2b6
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-30 14:49:40 -0300

    Arquivos aula 02 python com banco de dados

--- modulo02-python-com-banco-de-dados/20241130-aula02/1fn.sql
+++ modulo02-python-com-banco-de-dados/20241130-aula02/1fn.sql
@@ -42,3 +42,42 @@ INSERT INTO tb_clientes (nome, endereco, telefone) VALUES (
     "Rua dos Bandeirantes, 240, Centro, Pomerode, SC",
     "47992014544"
 );
+
+# Consulta a tabela tb_clientes
+SELECT * FROM tb_clientes;
+
+/*
+ * A tabela tb_clientes não está na Primeira Forma Normal (1FN), pois:
+ * A coluna 'endereco' não é indivisível (pode ser quebrada em outras colunas)
+ * A coluna 'telefone' possui alguns registros multivalorados
+*/
+
+# Criando a tabela tb_clientes seguindo as regras da 1FN
+
+# Vamos renomear a tabela fora das normas
+RENAME TABLE tb_clientes TO tb_clientes_pre_1fn;
+
+# Criar a tabela tb_clientes seguindo as regras da 1FN
+CREATE TABLE IF NOT EXISTS tb_clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    tipo_logradouro VARCHAR(50) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf CHAR(2) NOT NULL
+);
+
+DESC tb_clientes;
+
+# Como a coluna telefone é multivalorada, precisamos criar uma
+# nova tabela para armazenar esse tipo de informação
+
+# Criação da tabela tb_telefones
+CREATE TABLE IF NOT EXISTS tb_telefones(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id INT NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    FOREIGN KEY(cliente_id) REFERENCES tb_clientes(id)
+);
