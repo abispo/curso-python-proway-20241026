554c3f5912388ef856745c12b7feb3d18829fdd2
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-30 14:20:57 -0300

    Arquivos aula 02 python com banco de dados

--- modulo02-python-com-banco-de-dados/20241130-aula02/1fn.sql
+++ modulo02-python-com-banco-de-dados/20241130-aula02/1fn.sql
@@ -9,4 +9,36 @@ valores não compostos.
 um valor na mesma coluna.
 */
 
+# Cria o banco de dados 20241130_modulo02_python caso ele não exista
 CREATE DATABASE IF NOT EXISTS 20241130_modulo02_python;
+
+# Define o banco de dados onde os comandos SQL serão executados
+USE 20241130_modulo02_python;
+
+# Criação da tabela tb_clientes
+CREATE TABLE IF NOT EXISTS tb_clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(200) NOT NULL,
+    telefone VARCHAR(100) NOT NULL
+);
+
+# O comando DESC descreve a estrutura da tabela
+DESC tb_clientes;
+
+# Inserção de dados na tabela tb_clientes
+INSERT INTO tb_clientes (nome, endereco, telefone) VALUES (
+    "João da Silva",
+    "Rua XV de Novembro, 1000, Centro, Blumenau, SC",
+    "47923456789"
+);
+INSERT INTO tb_clientes (nome, endereco, telefone) VALUES (
+    "Neide Carvalho",
+    "Praça da Liberdade, 12, Liberdade, São Paulo, SP",
+    "11957483902,11933334501"
+);
+INSERT INTO tb_clientes (nome, endereco, telefone) VALUES (
+    "Maria Souza",
+    "Rua dos Bandeirantes, 240, Centro, Pomerode, SC",
+    "47992014544"
+);
