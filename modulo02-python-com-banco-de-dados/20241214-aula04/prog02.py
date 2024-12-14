d8b15c4e9a5588f427a353e3787a02e6ad45b437
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-12-14 16:21:16 -0300

    Arquivos aula 04 python com banco de dados

--- modulo02-python-com-banco-de-dados/20241214-aula04/prog02.py
+++ modulo02-python-com-banco-de-dados/20241214-aula04/prog02.py
@@ -6,3 +6,13 @@ Herança
 
 A Herança ocorre quando uma classe filha(ou subclasse) herda atributos e métodos de uma classe mãe (ou superclasse).
 """
+
+from contas import ContaCorrente, ContaInvestimento
+
+if __name__ == "__main__":
+
+    conta_viacredi = ContaCorrente(nome="Conta Viacredi")
+    conta_viacredi.depositar(100)
+    print(f"Saldo da conta '{conta_viacredi.nome}': {conta_viacredi.saldo}")
+    conta_viacredi.sacar(55)
+    print(f"Saldo da conta '{conta_viacredi.nome}': {conta_viacredi.saldo}")
