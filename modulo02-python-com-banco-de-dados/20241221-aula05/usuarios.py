c2c16e77ef4915c373376ba13c71b99644ed3161
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-12-21 16:01:03 -0300

    Arquivos aula 05 python com banco de dados

--- modulo02-python-com-banco-de-dados/20241221-aula05/usuarios.py
+++ modulo02-python-com-banco-de-dados/20241221-aula05/usuarios.py
@@ -4,6 +4,21 @@ from config import session
 from mensagens import MENU_USUARIOS
 from models import Usuario, Perfil
 
+def inserir_usuario(email, senha, nome, sobrenome, data_de_nascimento) -> None:
+    # Para inserção de um dado na tabela, precisamos instanciar a model, informando os valores dos seus atributos
+    usuario = Usuario(email=email, senha=senha)
+    
+    # Após isso, adicionamos o objeto instanciado na sessão. É importante reforçar que nesse momento os dados ainda não foram salvos fisicamente na tabela, portanto, podem receber um comando de rollback (desfazer). Caro desejarmos adicionar mais de 1 objeto ao mesmo tempo, podemos utilizar o método add_all, que recebe uma lista de objetos
+    session.add(usuario)
+
+    # O método commit() do objeto session, pega todos os objetos que foram adicionados à sessão e os persiste (salva/atualiza) nas suas tabelas correspondentes
+    session.commit()
+
+    # Salvando os dados do perfil associado a esse usuário
+    perfil = Perfil(nome=nome, sobrenome=sobrenome, data_de_nascimento=data_de_nascimento)
+    session.add(perfil)
+    session.commit()
+
 def gerenciar_usuarios():
     
     while True:
@@ -14,3 +29,23 @@ def gerenciar_usuarios():
             case 1:
                 print("VOLTANDO AO MENU PRINCIPAL...")
                 break
+
+            case 2:
+                pass
+
+            case 3:
+                email = input("Informe o e-mail do usuário: ")
+                senha = input("Informa a senha do usuário: ")
+                nome = input("Informe o primeiro nome do usuário: ")
+                sobrenome = input("Informe o sobrenome do usuário: ")
+                data_de_nascimento = input("Informe a data de nascimento do usuário (YYYY-MM-DD): ")
+
+                inserir_usuario(
+                    email=email,
+                    senha=senha,
+                    nome=nome,
+                    sobrenome=sobrenome,
+                    data_de_nascimento=data_de_nascimento
+                )
+
+                print("USUÁRIO SALVO COM SUCESSO!")
